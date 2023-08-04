b9da2d08d111696efcf2518bad4293c4821a727b
Yash Karandikar <ynkarandikar@gmail.com>
2023-08-04 13:09:37 -0700

    implement React query name/image render
    finish React query string input to Flask API and use API output for game name/URL render in frontend

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 
 import './App.css';
 
@@ -16,7 +16,20 @@ function App() {
 
     const [backendData, setBackendData] = useState([{}]);
 
-    async function handleTransfer() {
+    const [recName, setRecName] = useState("");
+    const [recImg, setRecImg] = useState("");
+
+    // useEffect(() => { https://stackoverflow.com/questions/56247433/how-to-use-setstate-callback-on-react-hooks
+
+    // https://stackoverflow.com/questions/72113386/reactjs-how-do-you-call-an-async-fuction-on-button-click
+    // https://stackoverflow.com/questions/72113386/reactjs-how-do-you-call-an-async-fuction-on-button-click
+    // https://stackoverflow.com/questions/71174509/proper-async-await-syntax-for-fetching-data-using-useeffect-hook-in-react
+    // https://stackoverflow.com/questions/56663785/invalid-hook-call-hooks-can-only-be-called-inside-of-the-body-of-a-function-com
+
+     // https://stackoverflow.com/questions/40825010/const-or-let-in-react-component
+    let handleTransfer = useCallback( async () => {
+
+    // async function handleTransferHelper() {
         fetch('/query', {
             method: 'POST',
             headers: { "Content-Type": "application/json" },
@@ -26,16 +39,26 @@ function App() {
         })
           
         // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-        const response = await fetch("/query");
+        // https://stackoverflow.com/questions/54936559/using-async-await-in-react-component
+        const response = await fetch("/query"); 
         const games = await response.json();
             console.log(games);
-    }
-  
-        // fetch('/query').then(
-          // response => response.json()
-        // ).then(data => setBackendData(data)).then((data) => {
-           // console.log("GET: " + data)
-        // })
+             setRecName(games.game)
+             setRecImg(games.url)
+
+            
+            console.log(recName)
+            console.log(recImg)
+
+            console.log(games['game'])
+            console.log(games.game)
+            console.log(recName)
+            console.log(recImg)
+    })
+    // handleTransferHelper()
+    // }, []);
+
+    // https://stackoverflow.com/questions/51184136/display-an-image-from-url-in-reactjs
 
     return (
         
@@ -52,9 +75,27 @@ function App() {
         
         <button onClick={handleTransfer}>Transfer Data</button>
         <h3>{backendData.query_body}</h3>
+        <h3>{recName}</h3>
+        <img 
+      src={recImg}
+      alt="boxart"
+      />
         </div>
 
     );
+
+
 }
+
+            //useEffect(() => { setRecName(games.game) }, [])
+            //useEffect(() => { setRecImg(games.url) }, [])
+  
+        // fetch('/query').then(
+          // response => response.json()
+        // ).then(data => setBackendData(data)).then((data) => {
+           // console.log("GET: " + data)
+        // })
+
+    
 
 export default App;
