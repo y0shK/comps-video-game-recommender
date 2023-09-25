64f28bcb91c1df528e7b504d8e419638c6db118e
Yash Karandikar <ynkarandikar@gmail.com>
2023-09-24 22:41:44 -0700

    push changes for fpr/tpr trials & method cleanup

--- flask/pretrained_embedding.py
+++ flask/pretrained_embedding.py
@@ -5,9 +5,11 @@ Data: pretrained Word2Vec model
     word embeddings for X
     word embeddings for each game
     (name, deck, description)
-Output: game "similar" to X (based on meeting the float threshold for N word embedding attributes)
+Output: game "similar" to X
 
-Plan is to make a "recommendation" with word embeddings approach
+Evaluation metric: 
+Use GiantBomb "similar games" to reverse engineer recommendation process
+This transforms recommendation into a supervised learning project
 """
 
 import requests_cache
@@ -35,9 +37,9 @@ GIANTBOMB_API_KEY = os.getenv('GIANTBOMB_API_KEY')
 # input game X
 # QUERY = "klonoa door to phantomile"
 # QUERY = "super mario rpg"
-# QUERY = "vampire the masquerade" - "Alone in the Dark"
-# QUERY = "sid meier civilization iv" - gives reasonable answers (i.e., Sid Meier / Civ games)
-#QUERY = "final fantasy" # - # gives reasonable answers (i.e., final fantasy series games)
+# QUERY = "vampire the masquerade"
+# QUERY = "sid meier civilization iv"
+#QUERY = "final fantasy"
 # QUERY = "baldur's gate"
 QUERY = "super mario bros"
 
@@ -71,8 +73,6 @@ for i in range(min(num_results, 10)):
 if game_results == None:
     print("Input query game not found in API database")
     exit()
-
-# FIXME make sure the code fits the diagram
 
 query_name = game_results['name']
 game_deck = game_results['deck']
@@ -119,22 +119,31 @@ similar_game_names = []
 for i in range(len(similar_games_to_query)):
     similar_game_names.append(similar_games_to_query[i]['name'])
 
+# TODO do this
+# use api_detail_url in similar_games[i] to call game API on each similar game
+# then, store genres, franchises etc
+# append dict to games_dict
+# we have GUID from 
+print("similar games set trace")
 pdb.set_trace()
 
-game_genres = game_api_json['results']['genres']
-genre_list = []
-for i in range(len(game_genres)):
-    genre_list.append(game_genres[i]['name'])
+# get ground truth games to avoid division by zero error
+# FIXME proof of concept - get 3, or as many as there are, whichever is less
+# get all info about these games and add them to games_dict to ensure presence of ground truth
 
-game_themes = game_api_json['results']['themes']
-theme_list = []
-for i in range(len(game_themes)):
-    theme_list.append(game_themes[i]['name'])
 
-game_franchises = game_api_json['results']['franchises']
-franchise_list = []
-for i in range(len(game_franchises)):
-    franchise_list.append(game_franchises[i]['name'])
+
+def get_game_demographics(json_file, dict_key):
+    call = json_file['results'][dict_key]
+    call_list = []
+    for i in range(len(call)):
+        call_list.append(call[i]['name'])
+
+    return call_list
+
+genre_list = get_game_demographics(game_api_json, 'genres')
+theme_list = get_game_demographics(game_api_json, 'themes')
+franchise_list = get_game_demographics(game_api_json, 'franchises')
 
 pdb.set_trace()
 
@@ -166,6 +175,8 @@ Returns: game_data (dict): key=name and value=
     id, name, deck, description, genres, themes, franchises, release date, image
 """
 def get_game_info(api_key, headers, offset):
+
+    # FIXME make get_game_info come from giantbomb so ground truth recs will be present, or just get more
     game_url = "http://www.gamespot.com/api/games/?api_key=" + api_key + "&format=json" + \
     "&offset=" + str(offset)
     game_call = session.get(game_url, headers=headers) 
@@ -185,17 +196,21 @@ def get_game_info(api_key, headers, offset):
                              'image': game['image']}
     return game_data
 
-def get_games(api_key, headers, game_count=10):
+def get_games(api_key, headers, game_count=10, loop_offset=0):
     # https://stackoverflow.com/questions/38987/how-do-i-merge-two-dictionaries-in-a-single-expression-in-python
     games = {}
     for i in range(game_count):
-        new_games = get_game_info(api_key=api_key, headers=headers, offset=i*99)
+        new_offset = i*99 + loop_offset
+        new_games = get_game_info(api_key=api_key, headers=headers, offset=new_offset)
         games = {**games, **new_games}
         
-    return games
+    return [games, new_offset]
 
-games_dict = get_games(api_key=GAMESPOT_API_KEY, headers=HEADERS, game_count=20)
-games_test_dict = get_games(api_key=GAMESPOT_API_KEY, headers=HEADERS, game_count=10)
+games = get_games(api_key=GAMESPOT_API_KEY, headers=HEADERS, game_count=5, loop_offset=0)
+games_dict = games[0]
+offset = games[1]
+
+# games_test_dict = get_games(api_key=GAMESPOT_API_KEY, headers=HEADERS, game_count=20)
 #pdb.set_trace()
 
 """
@@ -205,20 +220,6 @@ Compare game X to every game in the dataset
 If the cosine similarity is greater than the threshold, return the game
 """
 
-# https://radimrehurek.com/gensim/models/keyedvectors.html
-# https://radimrehurek.com/gensim/auto_examples/tutorials/run_wmd.html
-
-# query_dict: query name, deck, description
-# go through each game in list and see which is best
-
-deck_data = list( \
-    list(set(tokenizer.tokenize(v['deck'].lower()))-stops) \
-    for v in games_dict.values() if tokenizer.tokenize(v['deck']) != [])
-
-desc_data = list( \
-    list(set(tokenizer.tokenize(v['description'].lower()))-stops) \
-    for v in games_dict.values() if tokenizer.tokenize(v['description']) != [])
-
 #pdb.set_trace()
 
 # data: use the pretrained model
@@ -227,160 +228,35 @@ model = gensim.downloader.load('glove-wiki-gigaword-50')
 #pdb.set_trace()
 
 sims = {}
-for k, v in games_dict.items():
+iterations = 4
+fpr_list = []
+tpr_list = []
 
-    # check name
-    if query_name == v['name'] or query_name in v['name']:
-        model_similarity = model.n_similarity(query_name, v['name'])
-        if model_similarity >= 0.8:
-            sims[k] = model_similarity
+for i in range(iterations): 
 
-    deck = list(set(tokenizer.tokenize(v['deck'])) - stops)
-    desc = list(set(tokenizer.tokenize(v['description'])) - stops) 
-    deck = [d.lower() for d in deck]
-    desc = [d.lower() for d in desc if d.isalpha()]
+    # games_dict = get_games(api_key=GAMESPOT_API_KEY, headers=HEADERS, game_count=30, loop_offset=0 + 2 * i)
 
-    this_genre = []
-    this_theme = []
-    this_franchise = []
+    # https://radimrehurek.com/gensim/models/keyedvectors.html
+    # https://radimrehurek.com/gensim/auto_examples/tutorials/run_wmd.html
 
-    genres = v['genres']
+    # query_dict: query name, deck, description
+    # go through each game in list and see which is best
 
-    for genre in genres:
-        for k1, v1 in genre.items():
-            this_genre.append(v1)
+    for k, v in games_dict.items():
 
-    themes = v['themes']
+        deck_data = list( \
+        list(set(tokenizer.tokenize(v['deck'].lower()))-stops) \
+        for v in games_dict.values() if tokenizer.tokenize(v['deck']) != [])
 
-    for theme in themes:
-        for k1, v1 in theme.items():
-            this_theme.append(v1)
+        desc_data = list( \
+        list(set(tokenizer.tokenize(v['description'].lower()))-stops) \
+        for v in games_dict.values() if tokenizer.tokenize(v['description']) != [])
 
-    franchises = v['franchises']
-
-    for franchise in franchises:
-        for k1, v1 in franchise.items():
-            this_franchise.append(v1)
-
-    # pdb.set_trace()
-
-    #pdb.set_trace()
-    
-    # check the cosine similarity between the tokenized descriptions to get related games
-
-    min_deck_tokens = min(len(query_deck_data), len(deck))
-    min_desc_tokens = min(len(query_desc_data), len(desc))
-
-    min_deck_tokens = max(min_deck_tokens, 5)
-    min_desc_tokens = max(min_desc_tokens, 5)
-
-   #  pdb.set_trace()
-
-    if len(query_desc_data) >= min_deck_tokens and len(deck) >= min_deck_tokens:
-        model_similarity = model.n_similarity(query_deck_data[0:min_deck_tokens], deck[0:min_deck_tokens])
-
-        if model_similarity >= 0.8:
-            sims[k] = model_similarity
-
-    if len(query_desc_data) >= min_desc_tokens and len(desc) >= min_desc_tokens:
-        model_similarity = model.n_similarity(query_desc_data[0:min_desc_tokens], desc[0:min_desc_tokens])
-        
-        if model_similarity >= 0.8:
-            sims[k] = model_similarity
-
-    # use genres, themes, and franchises
-    if len(genre_list) > 0 and len(this_genre) > 0:
-
-        for g in this_genre:
-            if g in genre_list:
-                model_similarity = model.n_similarity(genre_list[0], g)
-                if model_similarity >= 0.8:
-                    sims[k] = model_similarity
-                
-
-    if len(theme_list) > 0 and len(this_theme) > 0:
-        for g in this_theme:
-            if g in theme_list:
-                model_similarity = model.n_similarity(theme_list[0], g)
-                if model_similarity >= 0.8:
-                    sims[k] = model_similarity
-
-    if len(franchise_list) > 0 and len(this_franchise) > 0:
-        for g in this_franchise:
-            if g in franchise_list:
-                model_similarity = model.n_similarity(franchise_list[0], g)
-                if model_similarity >= 0.8:
-                    sims[k] = model_similarity
-
-    #pdb.set_trace()
-
-    # pdb.set_trace()
-
-print(sims)
-max_similiarity = max(sims.values())
-
-print("look for similar games manually")
-pdb.set_trace()
-
-topX = 5
-count = 0
-for k, v in sims.items():
-    if v == max_similiarity and count < topX:
-        print(k, v)
-        count += 1
-
-# calculate precision and recall
-# precision - correctly recommended items / total recommended items
-# recall - correctly recommended items / total useful recommended items
-# Let "useful" recommended items be any item that has the same genre, theme, or franchise
-
-# PR definition
-# https://www.sciencedirect.com/science/article/pii/S1110866515000341#b0435
-
-ground_truth_recs = [i for i in similar_game_names if i in games_dict.keys()]
-recommender_results = [i for i in similar_game_names if i in sims]  
-total_recs = [i for i in sims.keys()]
-
-# precision = correct recs in ground truth
-# recall = correct recs / total recs
-precision = min(len(recommender_results) / len(ground_truth_recs), 1)
-recall = min(len(recommender_results) / len(total_recs), 1)
-
-print("precision, recall")
-print(precision)
-print(recall)
-
-# TODO try ROC curve to see what happens as precision/recall tradeoff is made
-
-recs = list(sims.keys())
-
-tp_items = [i for i in recs if i in ground_truth_recs]
-fp_items = [i for i in recs if i not in ground_truth_recs]
-fn_items = [i for i in list(games_dict.keys()) if i not in recs and i in ground_truth_recs]
-tn_items = [i for i in list(games_dict.keys()) if i not in recs and i not in ground_truth_recs]
-        
-fpr = len(fp_items) / (len(fp_items) + len(tn_items))
-tpr = len(tp_items) / (len(tp_items) + len(fn_items))
-
-# pdb.set_trace()
-
-fpr_vals = [fpr]
-tpr_vals = [tpr]
-
-plt.plot(fpr, tpr)
-plt.show()
-
-for i in range(1, 4):
-    games_test_dict = get_games(api_key=GAMESPOT_API_KEY, headers=HEADERS, game_count=20 + 10 * i)
-
-    sims = {}
-    for k, v in games_test_dict.items():
-
-    # check name
+        # check name
         if query_name == v['name'] or query_name in v['name']:
-            model_similarity = model.n_similarity(query_name, v['name'])
-            if model_similarity >= 0.8:
-                sims[k] = model_similarity
+            model_sim = model.n_similarity(query_name, v['name'])
+            if model_sim >= 0.8:
+                sims[k] = model_sim
 
         deck = list(set(tokenizer.tokenize(v['deck'])) - stops)
         desc = list(set(tokenizer.tokenize(v['description'])) - stops) 
@@ -412,7 +288,7 @@ for i in range(1, 4):
         # pdb.set_trace()
 
         #pdb.set_trace()
-        
+
         # check the cosine similarity between the tokenized descriptions to get related games
 
         min_deck_tokens = min(len(query_deck_data), len(deck))
@@ -421,60 +297,69 @@ for i in range(1, 4):
         min_deck_tokens = max(min_deck_tokens, 5)
         min_desc_tokens = max(min_desc_tokens, 5)
 
-    #  pdb.set_trace()
+        #  pdb.set_trace()
 
         if len(query_desc_data) >= min_deck_tokens and len(deck) >= min_deck_tokens:
-            model_similarity = model.n_similarity(query_deck_data[0:min_deck_tokens], deck[0:min_deck_tokens])
+            model_sim = model.n_similarity(query_deck_data[0:min_deck_tokens], deck[0:min_deck_tokens])
 
-            if model_similarity >= 0.8:
-                sims[k] = model_similarity
+            if model_sim >= 0.8:
+                sims[k] = model_sim
 
         if len(query_desc_data) >= min_desc_tokens and len(desc) >= min_desc_tokens:
-            model_similarity = model.n_similarity(query_desc_data[0:min_desc_tokens], desc[0:min_desc_tokens])
+            model_sim = model.n_similarity(query_desc_data[0:min_desc_tokens], desc[0:min_desc_tokens])
             
-            if model_similarity >= 0.8:
-                sims[k] = model_similarity
+            if model_sim >= 0.8:
+                sims[k] = model_sim
 
         # use genres, themes, and franchises
         if len(genre_list) > 0 and len(this_genre) > 0:
 
             for g in this_genre:
                 if g in genre_list:
-                    model_similarity = model.n_similarity(genre_list[0], g)
-                    if model_similarity >= 0.8:
-                        sims[k] = model_similarity
+                    model_sim = model.n_similarity(genre_list[0], g)
+                    if model_sim >= 0.8:
+                        sims[k] = model_sim
                     
 
         if len(theme_list) > 0 and len(this_theme) > 0:
             for g in this_theme:
                 if g in theme_list:
-                    model_similarity = model.n_similarity(theme_list[0], g)
-                    if model_similarity >= 0.8:
-                        sims[k] = model_similarity
+                    model_sim = model.n_similarity(theme_list[0], g)
+                    if model_sim >= 0.8:
+                        sims[k] = model_sim
 
         if len(franchise_list) > 0 and len(this_franchise) > 0:
             for g in this_franchise:
                 if g in franchise_list:
-                    model_similarity = model.n_similarity(franchise_list[0], g)
-                    if model_similarity >= 0.8:
-                        sims[k] = model_similarity
+                    model_sim = model.n_similarity(franchise_list[0], g)
+                    if model_sim >= 0.8:
+                        sims[k] = model_sim
 
         #pdb.set_trace()
 
         # pdb.set_trace()
 
+        # print(sims)
+        max_sim = max(sims.values())
+
+        # print("look for similar games manually")
+        # pdb.set_trace()
+
+        topX = 5
+        count = 0
+        for k, v in sims.items():
+            if v == max_sim and count < topX:
+                # print(k, v)
+                count += 1
+        
+        # pdb.set_trace()
+    print("sims so far")
     print(sims)
-    max_similiarity = max(sims.values())
+    pdb.set_trace()
+    games = get_games(api_key=GAMESPOT_API_KEY, headers=HEADERS, game_count=2, loop_offset=offset)
+    games_dict = games[0]
+    offset = games[1]
 
-    print("look for similar games manually")
-    # pdb.set_trace()
-
-    topX = 5
-    count = 0
-    for k, v in sims.items():
-        if v == max_similiarity and count < topX:
-            print(k, v)
-            count += 1
 
     # calculate precision and recall
     # precision - correctly recommended items / total recommended items
@@ -505,25 +390,19 @@ for i in range(1, 4):
     fp_items = [i for i in recs if i not in ground_truth_recs]
     fn_items = [i for i in list(games_dict.keys()) if i not in recs and i in ground_truth_recs]
     tn_items = [i for i in list(games_dict.keys()) if i not in recs and i not in ground_truth_recs]
-            
-    # https://stackoverflow.com/questions/41757653/how-to-compute-aucarea-under-curve-for-recommendation-system-evaluation
+        
     fpr = len(fp_items) / (len(fp_items) + len(tn_items))
     tpr = len(tp_items) / (len(tp_items) + len(fn_items))
 
+    fpr_list.append(fpr)
+    tpr_list.append(tpr)
+
     pdb.set_trace()
 
-    fpr_vals.append(fpr)
-    tpr_vals.append(tpr)
-
-# before, didn't have evaluation metrics.
-# now, use supervised learning to say that similar_games are ground_truth and use PR based on that
-# currently have PR and ROC curve
-# TODO
-# 1. optimize for the metric. reduce FPR (false positive rate) and keep TPR high
-# 2. reduce total recommendations so that entire universe isn't recommended
-# 3. continue algorithm to improve performance
-# other metrics not necessarily needed
-
-plt.plot(fpr_vals, tpr_vals)
-plt.show()
 # pdb.set_trace()
+
+plt.plot(fpr_list, tpr_list)
+plt.show()
+
+end_time = time.time() - time
+print("seconds: ", end_time)
